7b0aec1d9f51bbb5954afbae9af184153e5484a9
Gecx <gechangxing@qijian-tech.com>
2023-12-14 14:13:55 +0800

    修改配置

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+// 解决 ts 无法识别 vue文件的问题
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue';
+    const vueComponent: DefineComponent<{}, {}, any>;
+    export default vueComponent;
+}
