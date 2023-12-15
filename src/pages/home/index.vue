c322f509a32df97586041a6b778dbf1d9fc82014
Gecx <gechangxing@qijian-tech.com>
2023-12-15 16:29:34 +0800

    home路由模块静态及主要功能完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -39,7 +39,7 @@
           />
         </div>
       </el-col>
-      <el-col :span="4">xxx</el-col>
+      <el-col :span="4"><Tip/></el-col>
     </el-row>
   </div>
 </template>
@@ -50,6 +50,7 @@ import Search from "./search/index.vue";
 import Level from "./level/index.vue";
 import Region from "./region/index.vue";
 import Card from "./card/index.vue";
+import Tip from "./tip/index.vue";
 import { onMounted, ref } from "vue";
 import { reqHospital } from "@/api/home/index";
 import { Content, HospitalResponseData } from "@/api/home/type";
